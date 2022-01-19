f242d1bca2f2fced6c5184ea06cb7b7708c0f02a
Abdirahman <abdi@Abdirahmans-MacBook-Pro.local>
2022-01-19 13:40:39 -0500

    created interviewer list item component

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./InterviewerListItem.scss";
+import classNames from "classnames";
+
+export default function InterviewerListItem(props) {
+  const InterviewerClasses = classNames("interviewers__item", {
+    "interviewers__item--selected": props.selected,
+  });
+  return (
+    <li
+      className={InterviewerClasses}
+      onClick={() => props.setInterviewer(props.id)}
+    >
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt="Sylvia Palmer"
+      />
+      {props.selected ? props.name : ""}
+    </li>
+  );
+}
