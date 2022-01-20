2c905373b28368db947c37f1e2e9f330ec8ec334
Abdirahman <abdi@Abdirahmans-MacBook-Pro.local>
2022-01-20 18:13:51 -0500

    created getappointmentforday function

---
+++ src/helpers/selectors.js
@@ -0,0 +1,13 @@
+export function getAppointmentsForDay(state, day) {
+  var result = [];
+  for (const key of state.days) {
+    if (key.name === day) {
+      for (const key2 of key.appointments) {
+        if (state.appointments[key2]) {
+          result.push(state.appointments[key2]);
+        }
+      }
+    }
+  }
+  return result;
+}
