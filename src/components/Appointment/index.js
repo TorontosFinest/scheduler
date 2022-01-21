ceb812399fa3e0eeb267e3a8873775d30d8359cc
Abdirahman <abdi@Abdirahmans-MacBook-Pro.local>
2022-01-21 18:08:19 -0500

    used SHOW , EMPTY, CREATE  custom hooks

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,20 +2,28 @@ import React, { Fragment } from "react";
 
 import Header from "./Header";
 import Show from "./Show";
+import Form from "./Form";
 import Empty from "./Empty";
 import "./styles.scss";
+import useVisualMode from "../../hooks/useVisualMode";
 
 export default function Appointment(props) {
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ? (
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === CREATE && <Form interviewers={[]} onCancel={() => back()} />}
+      {mode === SHOW && (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
         />
-      ) : (
-        <Empty onAdd={props.onAdd} />
       )}
     </article>
   );
